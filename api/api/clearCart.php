2f0d89bef463fb92ce07c0afd30b0df63705a4be
Creat112 <sasaabdelhady38@gmail.com>
2026-05-26 21:06:52 +0300

    Update CORS headers and fix syntax errors

--- api/api/clearCart.php
+++ api/api/clearCart.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, x-user-id");
 header("Content-Type: application/json");
 
 require_once 'config.php';
--- api/clearCart.php
+++ api/clearCart.php
@@ -1,8 +1,13 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, x-user-id');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 include('DBHelper.php');
 include('Response.php');
@@ -29,7 +34,7 @@ try {
 
     } else {
 
-        ]);
+        Response::error('Failed to clear cart', 500);
 
     }
 
